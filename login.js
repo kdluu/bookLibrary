19cca497becdc9312c18fa18f1a923ed9f196ecc
kdluu <khanhluu101@gmail.com>
2017-10-30 14:20:27 -0500

    old version of login.js

--- login.js
+++ login.js
@@ -8,9 +8,7 @@ $(document).ready(function () {
     if (username =='' || password == ''){
       $('input[type="text"],input[type="password"]').css("border","2px solid red");
       $('input[type="text"],input[type="password"]').css("box-shadow","0 0 3px red");
-      // $("#email_error").remove(); // Create this "email_error"and call remove before after, to prevent it adding multiples time
-      // $("#usernameID").after("<b id='email_error'> User name can not be blank </b>");
-      $( "span" ).text( " You have entered an invalid username or password.!" ).show().fadeOut( 5000 );
+      $( "span" ).text( " You have entered an invalid username or password.!" ).show().fadeOut( 3000 );
 
     }
 
