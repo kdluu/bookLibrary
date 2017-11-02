4f18386e628316699d7ba646be72e3c5e3316106
kdluu <khanhluu101@gmail.com>
2017-11-02 02:18:52 -0500

    Divided booksLirary into 2 seperate page for admin and student.

--- booksLibrary.js
+++ booksLibrary.js
@@ -1,48 +1,85 @@
-$(document).ready(function () {
-    $.ajax({
-      type: 'GET',
-      url: 'getbooks.php',
-      async: true,
-      data: { },
-      dataType: 'json',
-      success: function (data) {
-        var book_data = '';
-        $.each(data, function(key, value){
-          book_data += '<tr>';
-          book_data += '<td>' +value.Art+ '</td>';
-          book_data += '<td>' +value.Sport+ '</td>';
-          book_data += '<td>' +value.Science+ '</td>';
-          book_data += '<td>' +value.Literature+ '</td>';
-        });
-        $('#library').append(book_data);
-      },
-      error: function (jqXHR, textStatus, error) { }
-    });
-    $count = 0;
-    $book =[];
-    $('#library').on('click','td', function (event) {
-      event.preventDefault();
-        //  alert('Column :'+$(this).html().trim()); // Cell value
-       //alert('Column :'+$(this).index()); //Column index NOTE: Start from 0
-       //alert('Row:'+$(this).parent().index());//Row index
-        // alert('Row:'+$(this).parent().find('td').html().trim();//Row name
-        // alert('Column:'+$('#library thead tr th').eq($(this).index()).html().trim());//Header name
-            //$(this).toggleClass("green-cell");
-            //for ($i = 0; $i < 2; $i++)
-            $count ++;
-            if ($count > 2)
-            {
-              alert("Student can only borrow 2 books at a time");
-              $(this).css("background-color","#ffffff");
 
-            }
-            else {
-              $(this).css("background-color","#3CB371");
-              localStorage.setItem("book", $(this).html().trim());
-            }
-
-
-    })
-
+$(document).ready(function (){
+  var username = localStorage.username;
+  var password = localStorage.password;
+  if (username == 'admin') {
+    createAdmintable();
+  }
+  if (username.charAt(0) == 'u' ) {
+    createStudenttable();
+  }
 
 });
+function createAdmintable() {
+  $(document).ready(function () {
+      $.ajax({
+        type: 'GET',
+        url: 'getbooks.php',
+        async: true,
+        data: { },
+        dataType: 'json',
+        success: function (data) {
+          var book_data = '';
+          $.each(data, function(key, value){
+            book_data += '<tr>';
+            book_data += '<td>' +value.Art+ '</td>';
+            book_data += '<td>' +value.Sport+ '</td>';
+            book_data += '<td>' +value.Science+ '</td>';
+            book_data += '<td>' +value.Literature+ '</td>';
+          });
+          $('#library').append(book_data);
+          //TODO: Create Add Book For admin
+          // ID: book_nameID,shelf_ID,btnID
+          var addbook = '<input type="text" name="book_name" placeholder="Book Name" id="book_nameID">';
+          addbook += '<input type="text" name="shelf" placeholder="Shelf (Art, Sport, Science, Literature)" id="shelf_ID">';
+          addbook +='<button type="button" name="add_book" id="btnID">Add Book</button> ';
+          $('#addbook').append(addbook);
+        },
+        error: function (jqXHR, textStatus, error) { }
+      });
+  });
+
+
+
+}
+function createStudenttable() {
+  $(document).ready(function () {
+      $.ajax({
+        type: 'GET',
+        url: 'getbooks.php',
+        async: true,
+        data: { },
+        dataType: 'json',
+        success: function (data) {
+          var book_data = '';
+          $.each(data, function(key, value){
+            book_data += '<tr>';
+            book_data += '<td>' +value.Art+ '</td>';
+            book_data += '<td>' +value.Sport+ '</td>';
+            book_data += '<td>' +value.Science+ '</td>';
+            book_data += '<td>' +value.Literature+ '</td>';
+          });
+          $('#library').append(book_data);
+        },
+        error: function (jqXHR, textStatus, error) { }
+      });
+      $count = 0;
+      $book =[];
+      $('#library').on('click','td', function (event) {
+        event.preventDefault();
+
+              $count ++;
+              if ($count > 2)
+              {
+                alert("Student can only borrow 2 books at a time");
+                $(this).css("background-color","#ffffff");
+              }
+              else {
+                $(this).css("background-color","#3CB371");
+                localStorage.setItem("book", $(this).html().trim());
+              }
+      })
+
+
+  });
+}
--- login.js
+++ login.js
@@ -1,14 +1,21 @@
 $(document).ready(function() {
   $('#login').click(function() {
     //TODO: Get username and password
-    var username = $('#usernameID').val();
-    var password = $('#passwordID').val();
-    if (username == '' || password == '') {
-      alert('You have entered invalid username or password');
-      // $('span').text(' You have entered an invalid username or password.!').show().fadeOut(5000);
-    }
+    var username = $('#usernameID').val().toLowerCase();
+    var password = $('#passwordID').val().toLowerCase();
+    localStorage.setItem ("username",username); // Since we just need username
+    //Direct user to booksLibrary when have correct validentials.
     if (username == 'admin' || password == 'admin') {
       window.location.href = 'booksLibrary.html';
+      localStorage.setItem ("username",username);
     }
+    else if (username.charAt(0) == 'u') {
+      window.location.href = 'booksLibrary.html';
+    }
+    else {
+        alert('You have entered invalid username or password');
+      }
+
+
   });
 });
